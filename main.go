d8b14669d9f551ab640af8e3af73ca1b7c94476e
Paulo Pinto <paulo.pinto@automattic.com>
2026-03-20 18:06:24 +0000

    Init fyne app

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"os"
-
-	"roger/internal/tui"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	os.Exit(tui.Run())
+	a := app.New()
+	w := a.NewWindow("Hello World")
+
+	w.SetContent(widget.NewLabel("Hello World!"))
+	w.ShowAndRun()
 }
